dc0e7fd55c739b37f3d0201356a8222539bf7f2d
PabloGoesBrrr <75310636+PabloGoesBrrr@users.noreply.github.com>
2022-04-01 09:22:39 -0300

    Create viewall_city.php

---
+++ viewall_city.php
@@ -0,0 +1,30 @@
+<?php
+include 'connect.php';
+include'checklogin.php';
+?>
+<table border='1'>
+    <tr>
+        <th>
+            Name
+        </th>
+        <th>
+            Username
+        </th>
+    </tr>
+
+<?php
+$sq="select * from city";
+$qu=mysqli_query($con,$sq);
+while($f=  mysqli_fetch_assoc($qu)){
+    ?>
+    <tr>
+        <td>
+            <?php echo $f['idCity']?>
+        </td>
+        <td>
+            <?php echo $f['nameCity']?>
+        </td>
+    </tr>
+    <?php
+}
+?>
